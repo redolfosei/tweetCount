595e34f3e70beba1e0c7d4a58e85187ead55925a
redolf <redolf639@gmail.com>
2022-06-20 20:09:00 +0000

    mixed code for texting

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
+/*
+Dog Age to Human Age App. 
+Dog Age to Human Age Formula 
+humanAge = (dogAge - 2) X 4 + 21
+*/
+
+
+function humanAge() {
+    var dogName = prompt("What is your dog's name? ");
+    var dogAge = prompt ("What is your dog's age? ");
+    humanAge = (dogAge - 2) * 4 + 21;
+
+    alert("Hey " + dogName + " your human age is " + humanAge);
+}
+
+//Welcome a user with his name being in the form of a title case. eg Hello Alex
+
+/*
+function welcomeHome2() {
+
+    var firstName = prompt("What is your name?");
+
+    var firstLetter = firstName.slice(0,1);
+    var upperCaseFirstLetter = firstLetter.toUpperCase();
+
+    var restOfLetters = firstName.slice(1,firstName.length);
+    var lowerCaseRestOfLetters = restOfLetters.toLowerCase();
+
+    var titleCaseLetters = upperCaseFirstLetter + lowerCaseRestOfLetters;
+    alert("Hello " + titleCaseLetters);
+}
+*/
+
+/*
+function welcomeHome() {
+    var firstName = prompt("What is you name?");
+    alert("Welcome " + firstName.slice(0,1).toUpperCase() + firstName.slice(1,firstName.length).toLowerCase());
+}
+*/
+
+
+
 //You have written 7 characters, you have 43 characters left. 
 /*
 Tweet count 
 Slicing 
 */
 
+/*
 function tweetCount() {
     var totalcharss = 50; 
     var msg_left;
@@ -21,8 +64,19 @@ function tweetCount() {
     msg_left = totalcharss - strokess;
     alert("You have " + msg_left + " Characters... ");
 }
+*/
 
 /*
+function upperCasefunction(){
+    // alert("Hello buddy");
+    var firstName2 = "RedolF";
+    alert(firstName2.toUpperCase());
+    alert(firstName2.toLowerCase());
+}
+*/
+
+/*
+//Slicing
 var firstName = "Redolf";
 alert(firstName);
 var slicedName = firstName.slice(0,1);
